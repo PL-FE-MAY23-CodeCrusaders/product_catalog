443455f519d49994f708cc3372190985a43e3bad
MateuszSlezal <M4teks93@gmail.com>
2023-10-27 06:55:20 +0200

    dfdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { GoodsList } from './GoodsList';
+
+import { getAll, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
+// or
+// import * as goodsAPI from './api/goods';
+
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const loadAllGoods = () => {
+    getAll()
+      .then(listOfGoods => {
+        setGoods(listOfGoods);
+      });
+  };
 
 
 
+  
+  const load5firstGoods = () => {
+    getAll()
+      .then((listOfGoods) => {
+        listOfGoods.sort((a, b) => a.name.localeCompare(b.name));
 
-function App() {
+        const first5Goods = listOfGoods.slice(0, 5);
+
+        return first5Goods;
+      })
+      .then(listOfGoods => setGoods(listOfGoods));
+  };
+
+  const loadRedGoods = () => {
+    getRedGoods()
+      .then(listOfGoods => {
+        setGoods(listOfGoods);
+      });
+  };
+
+
   return (
-    
-   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to   reload.
-        </p>
-        <a
+      <h1>Dynamic list of Goods</h1>
 
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={loadAllGoods}
+      >
+        Load all goods
+      </button>
 
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={load5firstGoods}
+      >
+        Load 5 first goods
+      </button>
 
+      <button type="button" data-cy="red-button" onClick={loadRedGoods}>
+        Load red goods
+      </button>
 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoodsList goods={goods} />
     </div>
   );
-}
-
-export default App;
+};
