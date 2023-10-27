f64ebb445fe18e348f9d4c6885d4f0d4dd964e48
MateuszSlezal <M4teks93@gmail.com>
2023-10-27 07:32:59 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,26 @@
-import React, { useState } from 'react';
-import './App.scss';
-import { GoodsList } from './GoodsList';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { getAll, getRedGoods } from './api/goods';
-import { Good } from './types/Good';
-// or
-// import * as goodsAPI from './api/goods';
-
-export const App: React.FC = () => {
-  const [goods, setGoods] = useState<Good[]>([]);
-
-  const loadAllGoods = () => {
-    getAll()
-      .then(listOfGoods => {
-        setGoods(listOfGoods);
-      });
-  };
-
-
-
-
-
-  
-  const load5firstGoods = () => {
-    getAll()
-      .then((listOfGoods) => {
-        listOfGoods.sort((a, b) => a.name.localeCompare(b.name));
-
-        const first5Goods = listOfGoods.slice(0, 5);
-
-        return first5Goods;
-      })
-      .then(listOfGoods => setGoods(listOfGoods));
-  };
-
-  const loadRedGoods = () => {
-    getRedGoods()
-      .then(listOfGoods => {
-        setGoods(listOfGoods);
-      });
-  };
-
-
+function App() {
   return (
     <div className="App">
-      <h1>Dynamic list of Goods</h1>
-
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={loadAllGoods}
-      >
-        Load all goods
-      </button>
-
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={load5firstGoods}
-      >
-        Load 5 first goods
-      </button>
-
-      <button type="button" data-cy="red-button" onClick={loadRedGoods}>
-        Load red goods
-      </button>
-
-      <GoodsList goods={goods} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener            noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
+
+export default App;
