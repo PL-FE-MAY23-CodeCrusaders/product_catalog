f6c2c6845bddd003837a0dfa9ef363fd2e69350e
Capoverde <piotr_as@yahoo.com>
2023-11-02 20:28:39 +0100

    Card with not working yet dynamic data

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactImageMagnify from 'react-image-magnify';
-import './Card.scss';
 import addToFavouritesDefault from '../../images/addToFavouritesDefault.png';
 import addToFavouritesAdded from '../../images/addToFovouritesAdded.png';
 import phoneImg1 from '../../images/phoneImg1.png';
 import phoneImg1xl from '../../images/phoneImg1xl.png';
+import phoneData from '../../../public/api/phones.json'; // import pliku JSON
+import './Card.scss';
+
+interface PhoneData {
+  id: number;
+  name: string;
+  category: string;
+  phoneId: string;
+  itemId: string;
+  fullPrice: number;
+  price: number;
+  screen: string;
+  capacity: string;
+  color: string;
+  image: string;
+  ram: string;
+  year: number;
+}
 
 export const Card: React.FC = () => {
+  const [phoneData, setPhoneData] = useState<PhoneData | null>(null);
   const [isAddedToCart, setIsAddedToCart] = useState(false);
   const [isFavourite, setIsFavourite] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (phoneData) {
+      setPhoneData(phoneData); // Ustaw dane telefonu
+      setIsLoading(false);
+    } else {
+      console.error(
+        'Błąd pobierania danych: Dane telefonu są nieprawidłowe lub puste.'
+      );
+      setIsLoading(false);
+    }
+  }, []);
 
   const handleAddToCartClick = () => {
     setIsAddedToCart(!isAddedToCart);
@@ -18,6 +49,10 @@ export const Card: React.FC = () => {
     setIsFavourite(!isFavourite);
   };
 
+  if (isLoading) {
+    return <div>Ładowanie danych z api (jakiś loader tutaj) ...</div>;
+  }
+
   return (
     <div className="container">
       <div className="card">
@@ -25,41 +60,62 @@ export const Card: React.FC = () => {
           <ReactImageMagnify
             {...{
               smallImage: {
-                alt: 'Apple iPhone 14 Pro 128GB Silver (MQ023)',
+                alt: phoneData?.name || '',
                 isFluidWidth: true,
                 src: phoneImg1,
               },
               largeImage: {
+                width: 800,
+                height: 840,
                 src: phoneImg1xl,
-                width: 600,
-                height: 640,
               },
             }}
           />
         </div>
         <div className="card__title">
-          <h4>Apple iPhone 14 Pro 128GB Silver (MQ023)</h4>
+          {phoneData && phoneData.name ? (
+            <h4>{phoneData.name}</h4>
+          ) : (
+            <h4>Apple iPhone 11 64GB Yellow</h4>
+          )}
         </div>
         <div className="card__price">
-          <h3 className="card__price--title">
-            <span className="card__price--title-actual">&#36;799</span>
-            <span className="card__price--title-previous">
-              <s>&#36;899</s>
-            </span>
-          </h3>
+          {phoneData ? (
+            <h3 className="card__price--title">
+              <span className="card__price--title-actual">
+                &#36;{phoneData.price}
+              </span>
+              <span className="card__price--title-previous">
+                <s>&#36;{phoneData.fullPrice}</s>
+              </span>
+            </h3>
+          ) : (
+            <h3 className="card__price--title">
+              <span className="card__price--title-actual">&#36;799</span>
+              <span className="card__price--title-previous">
+                <s>&#36;999</s>
+              </span>
+            </h3>
+          )}
         </div>
         <div className="card__description">
           <div className="card__details">
             <div className="card__details-left">Screen</div>
-            <div className="card__details-right">16&quot; OLED</div>
+            <div className="card__details-right">
+              {phoneData ? phoneData.screen : '6.1" OLED'}
+            </div>
           </div>
           <div className="card__details">
             <div className="card__details-left">Capacity</div>
-            <div className="card__details-right">128 GB</div>
+            <div className="card__details-right">
+              {phoneData ? phoneData.capacity : '128 GB'}
+            </div>
           </div>
           <div className="card__details">
             <div className="card__details-left">RAM</div>
-            <div className="card__details-right">16 GB</div>
+            <div className="card__details-right">
+              {phoneData ? phoneData.ram : '64 GB'}
+            </div>
           </div>
         </div>
         <div className="card__buttons">
@@ -94,90 +150,3 @@ export const Card: React.FC = () => {
     </div>
   );
 };
-// ------------------- KOD POD DYNAMICZNE DANE ------------------- //
-
-// interface ProductData {
-//   title: string;
-//   price: number;
-//   previousPrice: number;
-//   screen: string;
-//   capacity: number;
-//   ram: number;
-// }
-
-// interface CardProps {
-//   productData: ProductData;
-// }
-
-// export const Card: React.FC<CardProps> = ({ productData }) => {
-//   const { title, price, previousPrice, screen, capacity, ram } = productData;
-
-// const [isAddedToCart, setIsAddedToCart] = useState(false);
-// const handleAddToCartClick = () => {
-//   setIsAddedToCart(!isAddedToCart);
-// };
-
-//   return (
-//     <div className={`container ${isAddedToCart ? 'added-to-cart' : ''}`}>
-//       <div className="card">
-//         <div className="card__img">
-//           <img
-//             className="img"
-//             src={require('../../images/phone_img_1.png')}
-//             alt={title}
-//           />
-//         </div>
-//         <div className="card__title">
-//         <h4>{title || `Apple iPhone 14 Pro 128GB Silver (MQ023)`}</h4>
-//         </div>
-//         <div className="card__price">
-//           <h3 className="card__price--title">
-//             <span className="card__price--title-actual">&#36;{price}</span>
-//             <span className="card__price--title-previous">
-//               <s>&#36;{previousPrice}</s>
-//             </span>
-//           </h3>
-//         </div>
-//         <div className="card__description">
-//           <div className="card__details">
-//             <div className="card__details-left">Screen</div>
-//             <div className="card__details-right">{screen} OLED</div>
-//           </div>
-//           <div className="card__details">
-//             <div className="card__details-left">Capacity</div>
-//             <div className="card__details-right">{capacity} GB</div>
-//           </div>
-//           <div className="card__details">
-//             <div className="card__details-left">RAM</div>
-//             <div className="card__details-right">{ram} GB</div>
-//           </div>
-//         </div>
-//         <div className="card__buttons">
-//           <button
-//             className={`card__buttons-left ${isAddedToCart ? 'added-to-cart' : ''}`}
-//             onClick={handleAddToCartClick}
-//           >
-//             {isAddedToCart ? 'Added to cart' : 'Add to cart'}
-//           <button
-//             className={`card__buttons-right ${
-//             isFavourite ? 'added-to-favourites' : ''
-//             }`}
-//             onClick={handleButtonClick}
-//           >
-//            <div className="favourites">
-//             <img
-//              src={
-//               isFavourite ? addToFavouritesAdded : addToFavouritesDefault
-//                }
-//              alt="Favourites"
-//              title={
-//              isFavourite ? 'Added to favourites' : 'Add to favourites'
-//               }
-//            />
-//           </div>
-//          </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
