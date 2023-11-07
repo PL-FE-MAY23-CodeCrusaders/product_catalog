6bac4b6d400b4c19f38f4987d44bd55c2f64d6a9
Capoverde <piotr_as@yahoo.com>
2023-11-07 17:58:06 +0100

    Banner in progress

---
+++ src/features/homePage/components/BannerSlider/BannerSlider.tsx
@@ -0,0 +1,30 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Banner from './images/Banner.png';
+import './BannerSlider.scss';
+
+export const BannerSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <img src={Banner} alt="banner" />
+      </div>
+      <div>
+        <img src={Banner} alt="banner" />
+      </div>
+      <div>
+        <img src={Banner} alt="banner" />
+      </div>
+    </Slider>
+  );
+};
