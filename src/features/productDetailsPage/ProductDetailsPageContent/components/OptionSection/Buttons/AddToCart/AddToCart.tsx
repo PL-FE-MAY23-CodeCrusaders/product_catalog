221922ceeeb5f363e2c35c05eb793108a2e0ffb8
Hania Nassalska <hanna.nassalska@gmail.com>
2023-11-08 14:02:45 +0100

    AddToCart on phone details page

--- src/features/productDetailsPage/ProductDetailsPageContent/components/OptionSection/Buttons/AddToCart/AddToCart.tsx
+++ src/features/productDetailsPage/ProductDetailsPageContent/components/OptionSection/Buttons/AddToCart/AddToCart.tsx
@@ -4,10 +4,14 @@ import { useProductDetailsContext } from '../../../../../context/ProductDetailsC
 
 export const AddToCart = () => {
   const { phoneData, phonesData } = useProductDetailsContext();
-  const { addToCart } = useCartContext();
+  const { addToCart, cartState, removeFromCart } = useCartContext();
+
+  const isAddedToCart = cartState.find(p => p.phoneId === phoneData?.id);
 
   const handleAddToCartClick = (phoneId: string | undefined) => {
-    if (phoneId) {
+    if (phoneId && isAddedToCart) {
+      removeFromCart(phoneId);
+    } else {
       const phone = phonesData.filter((p) => p.phoneId === phoneId);
 
       addToCart(phone[0]);
@@ -17,7 +21,8 @@ export const AddToCart = () => {
   return (
     <button
       type="button"
-      className="add-button"
+      className={`add-button ${
+        isAddedToCart ? 'added-to-cart' : ''}`}
       onClick={() => handleAddToCartClick(phoneData?.id)}
     >
       Add to cart
