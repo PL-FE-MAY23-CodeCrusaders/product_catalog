d1a4c16c8877df7125f9f70e2f2cf67c5a54742c
Hania Nassalska <hanna.nassalska@gmail.com>
2023-11-09 13:15:44 +0100

    favouritesPage fixed

--- src/features/favouritesPage/FavouritesPage.tsx
+++ src/features/favouritesPage/FavouritesPage.tsx
@@ -2,12 +2,9 @@ import React, { useEffect } from 'react';
 import './FavouritesPage.scss';
 import FavSum from './favSum';
 import FavProductList from './favProductList';
-import { useFavContext } from '../../context/favContext/favContext';
 import { BackArrow } from '../../commonComponents/BackArrow';
 
 export const Favourites = () => {
-  const { removeFromFav, favState } = useFavContext();
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -16,12 +13,9 @@ export const Favourites = () => {
     <div className="fav">
       <BackArrow />
       <h1 className="fav__title">Favourites</h1>
-      <FavSum favState={favState} />
+      <FavSum />
       <div className="fav__contentBox">
-        <FavProductList
-          favState={favState}
-          removeFromFav={removeFromFav}
-        />
+        <FavProductList />
       </div>
     </div>
   );
